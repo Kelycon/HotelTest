e6493bc2f35b1d22a071ddbb411a8fb571df827d
Ruben <r-ben_1705@hotmail.com>
2019-05-01 10:51:49 +0100

    set debug false

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2p08zcbq-2u_z(n&phdm*-3ohr5#xw*8i3&yh26wvls@7b^zik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
